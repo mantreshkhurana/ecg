13a321bb90d08a7722dd32f6692d49afbec86818
Mantresh <mantreshkhurana@gmail.com>
2024-07-20 11:36:50 +0530

    `PlatformIO` Init

---
+++ ech-monitor/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void setup()
+{
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+}
+
+void loop()
+{
+  Serial.begin(9600);
+  if ((digitalRead(8) == 1) || (digitalRead(9) == 1))
+  {
+    Serial.println("");
+  }
+  else
+  {
+    Serial.println(analogRead(A0));
+  }
+  delay(100);
+  Serial.end();
+  delay(100);
+}
